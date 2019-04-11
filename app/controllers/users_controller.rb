5f915de55a97f8f20eccf8b2c4496c73c35e0be4
Jason Mei <jason.mei403@gmail.com>
2019-04-11 21:03:29 +0000

    add user index and show pages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -37,4 +37,15 @@ class UsersController < ApplicationController
     redirect "/"
   end
 
+  get '/users' do
+    redirect_if_not_logged_in
+    @users = User.all
+    erb :"users/index"
+  end
+
+  get '/users/:slug' do
+    redirect_if_not_logged_in
+    @user = User.find_by_slug(params[:slug])
+    erb :"users/show"
+  end
 end
